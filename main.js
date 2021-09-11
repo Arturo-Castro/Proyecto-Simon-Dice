75dcba45fe9b34b6ea0f3f5c328b1ebfcfb1c272
Arturo-Castro <castro.ah91@gmail.com>
2021-09-11 17:32:03 -0300

    agrega proyecto entero

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-const $botones = document.querySelectorAll('button');
+const $botones = document.querySelectorAll('.boton');
 let $botonesBot = [];
 let $botonesJugador;
 let aciertos;
 let ronda = 0;
+
+function resetearPartida(){
+    $botonesBot = []
+    $botonesJugador = []
+    aciertos = 0
+    ronda = 0
+}
+
+function llamarJugador(){
+    setTimeout(function(){
+        desbloquearTurno()
+        document.querySelector('#texto-turno').textContent = 'Es tu turno!' 
+    }, (1300*ronda))
+}
 
 function bloquearTurno(){
     $botones.forEach(function(boton){
@@ -18,6 +32,9 @@ function desbloquearTurno(){
 
 function empezarRondaBot(){
     ronda++
+    document.querySelector('.circulo').textContent = ronda
+    document.querySelector('#texto-turno').classList.remove('oculto')
+    document.querySelector('#texto-turno').textContent = 'Es el turno de la computadora...'
     bloquearTurno()
     $botonesJugador = []
     $botonesBot.push($botones[Math.floor(Math.random()* 4)])
@@ -26,20 +43,22 @@ function empezarRondaBot(){
                 cambiarColor(boton)
             }, (index + 1) * 1000)
     })
-    setTimeout(function(){
-        desbloquearTurno()
-    }, (1300*ronda))
+    llamarJugador()
 }
 
 
-function test(e){
+function empezarRondaJugador(e){
     aciertos = 0
     cambiarColor(e.target)   
     $botonesJugador.push(e.target)
     for (let i=0; i < $botonesJugador.length; i++){
         if (!($botonesBot[i] === $botonesJugador[i])){
-            alert('Perdiste!')
-            return
+            document.querySelector('#perder-audio').play()
+            document.querySelector('#titulo').textContent = 'Game over...'
+            document.querySelector('.boton-jugar').classList.remove('oculto')
+            document.querySelector('#texto-turno').classList.add('oculto')
+            bloquearTurno()
+            return 
         }else{
             aciertos++
         }    
@@ -51,14 +70,28 @@ function test(e){
 
 
 function cambiarColor(boton){
-    boton.classList.add('click')
-    setTimeout(function(){boton.classList.remove('click')}, 300)
+    const botonHighlight = boton.dataset.id 
+    boton.classList.add(`${botonHighlight}-highlight`)
+    setTimeout(function(){
+        boton.classList.remove(`${botonHighlight}-highlight`)
+    }, 300)
 }
 
 
+
 $botones.forEach(function(boton){
-    boton.onclick = test
+    boton.onclick = empezarRondaJugador
 })
 
-empezarRondaBot()
+document.querySelector('.boton-jugar').onclick = function(){
+    document.querySelector('#jugar-audio').play()
+    resetearPartida()
+    document.querySelector('#texto-turno').classList.remove('oculto')
+    document.querySelector('#titulo').textContent = 'Simon Dice'
+    document.querySelector('.boton-jugar').classList.add('oculto')
+    empezarRondaBot()
+}
+  
+
+
 
